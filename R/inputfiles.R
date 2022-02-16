396808f81bc31b1700ce06ce5af0c165dfbe479e
Remi Daigle <Remi.Daigle@dfo-mpo.gc.ca>
2022-02-16 10:58:43 -0400

    improve documentation

--- R/inputfiles.R
+++ R/inputfiles.R
@@ -77,42 +77,43 @@ setClass("marxanParams",
 
                    SAVESOLUTIONSMATRIX = "integer"))
 
-#' Create new Marxan Input Parameter File (input.dat)
+#' Create new Marxan Input Parameter File (input.dat). See the Marxan User Manual for more information:
+#' Serra, N., Kockel, A., Game, E. T., Grantham H., Possingham H.P., & McGowan, J. (2020). Marxan User Manual: For Marxan version 2.43 and above. The Nature Conservancy (TNC), Arlington, Virginia, United States and Pacific Marine Analysis and Research Association (PacMARA), Victoria, British Columbia, Canada.
 #'
-#' @param BLM
-#' @param PROP
-#' @param RANDSEED
-#' @param NUMREPS
-#' @param NUMITNS
-#' @param STARTTEMP
-#' @param NUMTEMP
-#' @param COSTTHRESH
-#' @param THRESHPEN1
-#' @param THRESHPEN2
-#' @param INPUTDIR
-#' @param PUNAME
-#' @param SPECNAME
-#' @param PUVSPRNAME
-#' @param BOUNDNAME
-#' @param SCENNAME
-#' @param SAVERUN
-#' @param SAVEBEST
-#' @param SAVESUMMARY
-#' @param SAVESCEN
-#' @param SAVETARGMET
-#' @param SAVESUMSOLN
-#' @param SAVEPENALTY
-#' @param SAVELOG
-#' @param OUTPUTDIR
-#' @param RUNMODE
-#' @param MISSLEVEL
-#' @param ITIMPTYPE
-#' @param HEURTYPE
-#' @param CLUMPTYPE
-#' @param VERBOSITY
-#' @param SAVESOLUTIONSMATRIX
+#' @param BLM Boundary Length Modifier
+#' @param PROP Proportion of planning units in initial reserve system
+#' @param RANDSEED Random seed number
+#' @param NUMREPS Number of repeat runs (or solutions)
+#' @param NUMITNS Number of iterations for annealing
+#' @param STARTTEMP Starting temperature for annealing
+#' @param NUMTEMP Number of temperature decreases for annealing
+#' @param COSTTHRESH Cost threshold
+#' @param THRESHPEN1 Size of cost threshold penalty
+#' @param THRESHPEN2 Shape of cost threshold penalty
+#' @param INPUTDIR Name of folder containing input data files
+#' @param PUNAME Name of Planning Unit File
+#' @param SPECNAME Name of Conservation Feature File
+#' @param PUVSPRNAME Name of Planning Unit versus Conservation Feature File
+#' @param BOUNDNAME Name of Boundary Length File
+#' @param SCENNAME Scenario name for the saved output file
+#' @param SAVERUN Save each run
+#' @param SAVEBEST Save the best run
+#' @param SAVESUMMARY Save summary information
+#' @param SAVESCEN Save scenario information
+#' @param SAVETARGMET Save targets met information
+#' @param SAVESUMSOLN Save summed solution information
+#' @param SAVEPENALTY Save computed feature penalties
+#' @param SAVELOG Save log files
+#' @param OUTPUTDIR Name of the folder in which to save output files
+#' @param RUNMODE Run option
+#' @param MISSLEVEL Species missing proportion
+#' @param ITIMPTYPE Iterative improvement
+#' @param HEURTYPE Heuristic
+#' @param CLUMPTYPE An integer code that sets the clumping rule. A value of 0L (default) is Partial clumps do not count– Clumps smaller than the target score nothing, 1L is Partial clumps count half– Clumps smaller than the target score half their amount, and 2L Graduated penalty– Score is proportional to the size of the clump
+#' @param VERBOSITY An integer code that sets screen printing options. A value of 0L is for Silent Running – Only the title of the program is displayed, 1L is Results Only – Marxan will display which run it is up to, the basic results of each run and the total run time, 2L is General Progress – In addition to the information about each run, Marxan will display information on the data that has been read in as well as details on any conservation features whose targets and requirements are such that they cannot be adequately reserved in the system, and 3L (default) is Detailed Progress – Shows exactly where the program is up to and gives the value of the system each time the temperature changes.
+#' @param SAVESOLUTIONSMATRIX An integer code to save planning units selected in for each run. A value of 0L will not save a file, 1L saves the file as .dat, 2L saves the file as .txt, and 3L (default) saves the file as .csv.
 #'
-#' @return
+#' @return A 'marxanParams' class object
 #' @export
 #'
 #' @examples
@@ -159,15 +160,15 @@ newParams <- function(BLM = 1,
                      VERBOSITY = 3L,
 
                      SAVESOLUTIONSMATRIX = 3L){
-  inputdat <- new('marxanParams')
+  inputdat <- methods::new('marxanParams')
 
-  for(s in slotNames(inputdat)){
-    if(class(get(s))==class(slot(inputdat,s))){
-      slot(inputdat,s) <- get(s)
+  for(s in methods::slotNames(inputdat)){
+    if(class(get(s))==class(methods::slot(inputdat,s))){
+      methods::slot(inputdat,s) <- get(s)
     } else {
-      warning(paste0("The value provided for '",s,"' is not of class: ", class(slot(inputdat,s)),
+      warning(paste0("The value provided for '",s,"' is not of class: ", class(methods::slot(inputdat,s)),
                      ". Attempting to coerce."))
-      slot(inputdat,s) <- as(get(s),class(slot(inputdat,s)))
+      methods::slot(inputdat,s) <- methods::as(get(s),class(methods::slot(inputdat,s)))
     }
 
   }
@@ -176,19 +177,20 @@ newParams <- function(BLM = 1,
 
 #' Write Marxan Input Parameter File (input.dat) to disk
 #'
-#' @param file
-#' @param inpudat
+#' @param file a character string naming the file
+#' @param inpudat a `marxanParams` class object to be written to disk. A `marxanParams` class object can be generated using `newParams()`
 #'
-#' @return
 #' @export
 #'
 #' @examples
 #' inputdat <- newParams(BLM = 1000)
 #' writeParams(inputdat,"input.dat")
-writeParams <- function(inpudat,file){
+writeParams <- function(inpudat = marxanr::newParams(),
+                        file = "input.dat"){
+  if(class(inputdat)!="marxanParams") stop("The value provided for 'inputdat' is not a 'marxanParams' object. A 'marxanParams' class object can be generated using `newParams()")
   writeLines(paste0(
 "This file was generated by the marxanr, ",
-packageVersion("marxanr"),
+utils::packageVersion("marxanr"),
 " package in ",
 R.version$version.string,"
 
