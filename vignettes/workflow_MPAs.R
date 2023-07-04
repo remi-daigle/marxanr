2f1436a81af3ceaad4d0a16b90cd397a084f1116
remi-daigle <daigleremi@gmail.com>
2023-07-04 09:32:48 -0300

    update the vignette docs

--- vignettes/workflow_MPAs.R
+++ vignettes/workflow_MPAs.R
@@ -47,3 +47,43 @@ head(bound)
 scen <- "MPAtest"
 
 
+## -----------------------------------------------------------------------------
+# create a folder for a new scenario
+createMarxanFolder(scen=scen,dialog = FALSE)
+
+# download marxan if necessary
+downloadMarxan(path = scen)
+
+# write marxan files to folder
+write.csv(spec,file.path(scen,"input","spec.dat"),quote = FALSE,row.names = FALSE)
+write.csv(puvspr,file.path(scen,"input","puvspr.dat"),quote = FALSE,row.names = FALSE)
+write.csv(pu_dat,file.path(scen,"input","pu.dat"),quote = FALSE,row.names = FALSE)
+write.csv(bound,file.path(scen,"input","bound.dat"),quote = FALSE,row.names = FALSE)
+
+# create new input parameters
+input <- newParams(BLM=10^5,NUMREPS = 50L,SCENNAME = scen)
+writeParams(inputdat = input,file = file.path(scen,"input.dat"))
+
+# run marxan
+runMarxan(marxanpath = scen,
+          inputdatfile = "input.dat")
+# unlink(scen,recursive = TRUE) # if necessary
+
+## -----------------------------------------------------------------------------
+ssoln <- read.csv(file.path(scen,"output",paste0(scen,"_ssoln.csv"))) %>%
+  mutate(planning_unit = as.character(planning_unit)) %>%
+  right_join(pu,by=c("planning_unit"="id")) %>% 
+  st_as_sf()
+basemap+
+  geom_sf(data = ssoln, aes(fill=number))+
+  ggcoords
+
+best <- read.csv(file.path(scen,"output",paste0(scen,"_best.csv"))) %>%
+  mutate(PUID = as.character(PUID)) %>%
+  right_join(pu,by=c("PUID"="id")) %>% 
+  st_as_sf()
+basemap+
+  geom_sf(data = best, aes(fill=SOLUTION))+
+  ggcoords
+
+
